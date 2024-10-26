4c6f89a0bc277a6023f252427e3d1e8d00e58dba
Sumafric <abelsum1330@gmail.com>
2024-10-27 02:50:45 +0300

    fix naming convention

---
+++ app/Models/Note.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class note extends Model
+{
+    use  HasFactory, HasUuids;
+    protected $guarded = [
+        'id',
+    ];
+/**
+* Get the user that owns the note.
+*/
+public function user()
+    {
+        return $this -> belongsTo(User::class);  
+    }
+}
